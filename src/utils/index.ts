e2e2230e5439ba6f74688add49e3bce0b91c7a70
SNRainiar <rainiar@foxmail.com>
2024-10-29 18:08:07 +0800

    chore: update index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,11 @@
 
 export const wrapBase = (href: string): string =>
   `${import.meta.env.BASE_URL}${href}`.replace(/\/+$/, '');
+
+export const range = (from: number, to: number, step: number = 1): number[] => {
+  let ret: number[] = [];
+  for (let i = from; i <= to; i += step) {
+    ret.push(i);
+  }
+  return ret;
+};
