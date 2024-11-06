5375e943f48c763881a63c5b8449f75312669980
SNRainiar <rainiar@foxmail.com>
2024-11-07 00:12:22 +0800

    chore: remove sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 import { resolve } from 'node:path';
 
 // Integrations
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/wocc2025',
-  integrations: [sitemap(), tailwind({ nesting: true })],
+  integrations: [tailwind({ nesting: true })],
   site: 'https://www.wocc.org',
   trailingSlash: 'never',
   vite: {
